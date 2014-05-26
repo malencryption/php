da6c6de5ecfb2d456095bc34a30a9d025a5546a5
sunshinedesign36 <runn3rgirl36@gmail.com>
2014-05-26 12:47:02 -0600

    setup local and env db

--- inventory/functions.php
+++ inventory/functions.php
@@ -2,22 +2,32 @@
 <?php
 session_start();
 function connInv() {
-	$server = 'localhost';
-    $username = 'leesaruz_iClient';
-    $password = 'Jd59wMUo';
-    $database = 'php_inventory';
-    $dsn = "mysql:host=$server; dbname=$database";
+
+if ($openShiftVar === null || $openShiftVar == ""){
+	//Not in the openshift environment
+	//echo "Using local credentials: ";
+	require 'setLocalDb.php';
+}
+    else {
+    	//In the openshift environment
+    	//echo "Using openshift credentials: ";
+
+    	$dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+    	$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
+    	$dbUser = getenv('OPENSHIFT_MYSQL_DB_PORT');
+    	$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+    }
+//echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br /> \n";
 
     try {
-        $connInv = new PDO($dsn, $username, $password);
-        return $connInv;
+    	$db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    return $db;
     } catch (PDOException $exc) {
         echo 'Sorry, the connection could not be built';
         exit;
     }
-
-    if (is_object($connInv)) {
-        return $connInv;
+    if (is_object($db)) {
+        return $db;
     } else {
         return FALSE;
     }
