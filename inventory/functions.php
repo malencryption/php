6e9d50905f5be6b8a077239962c965186caa0603
sunshinedesign36 <runn3rgirl36@gmail.com>
2014-05-26 18:02:24 -0600

    fix

--- inventory/functions.php
+++ inventory/functions.php
@@ -23,7 +23,7 @@ function connInv() {
 
       	$dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
       	$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
-      	$dbUser = getenv('OPENSHIFT_MYSQL_DB_USER');
+      	$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
       	$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
   }
   echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br /> \n";
