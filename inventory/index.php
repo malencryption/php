cd18bf8681c2528dcd889ae9a088b6d0fd2594c4
sunshinedesign36 <runn3rgirl36@gmail.com>
2014-06-02 12:17:41 -0600

    index problem

--- inventory/index.php
+++ inventory/index.php
@@ -35,7 +35,7 @@ elseif ($_POST['submit'] === 'Edit Business'){
 	$description = htmlspecialchars($_POST['description']);
 	$busId = $_SESSION['busId'];
 	$editBus = editBus($name, $description, $busId);
-		echo "$name, $description, $busId";
+	echo "$name, $description, $busId";
 	if ($editBus) {
 		$email = $_SESSION['user'];
 		$business = getBusNames($email);
@@ -90,37 +90,36 @@ elseif ($_POST['submit'] === 'Add Business'){
 	include 'selectbus.php';
 	exit;
 }
-else
-	if ($_POST['submit'] === "Update Inventory"){
-		$quantities = array();
-		$names = array();
-		$updated = array();
-		$items = $_SESSION['items'];
+elseif ($_POST['submit'] === "Update Inventory"){
+	$quantities = array();
+	$names = array();
+	$updated = array();
+	$items = $_SESSION['items'];
 	// var_dump($_SESSION);
 	// var_dump($_POST);
-		foreach ($items as $value){
-			$quantities[] = $_POST[$value['productId']];
-			$ids[] = $value['productId'];
-			$names[] = $value['name'];
-		}
+	foreach ($items as $value){
+		$quantities[] = $_POST[$value['productId']];
+		$ids[] = $value['productId'];
+		$names[] = $value['name'];
+	}
 	// var_dump($quantities);
 	// var_dump($ids);
-		$i = 0;
-		foreach ($ids as $value) {
-			$id = $value;
+	$i = 0;
+	foreach ($ids as $value) {
+		$id = $value;
 
-			$quantity = $quantities[$i];
-			$product = $names[$i];
+		$quantity = $quantities[$i];
+		$product = $names[$i];
 		// echo $product;
-			$i++;
-			$invUpdate = updateQuantity($id, $quantity);
-			if ($invUpdate) {
-				$updated[] = "$product updated"; 
-			}
-			else {
-				$updated[] = "$product not updated";
-			}
+		$i++;
+		$invUpdate = updateQuantity($id, $quantity);
+		if ($invUpdate) {
+			$updated[] = "$product updated"; 
 		}
+		else {
+			$updated[] = "$product not updated";
+		}
+	}
 	// var_dump($updated);
 	// $ids = 1;
 	// $quantities = 5;
@@ -128,77 +127,76 @@ else
 	// $invUpdate = updateInv($ids, $quantities);
 
 
-		$busName = $_SESSION['busName'];
-		$business=getBus($busName);
+	$busName = $_SESSION['busName'];
+	$business=getBus($busName);
 
-		if ($business) {
-			$invId = $business['inventoryId'];
-			$items = getItems($invId);
-			$_SESSION['items'] = $items;
-			if($items){
-				include 'invform.php';
-				exit;
-			}
-			else {
-				$error = 'could not get items';
-			}
+	if ($business) {
+		$invId = $business['inventoryId'];
+		$items = getItems($invId);
+		$_SESSION['items'] = $items;
+		if($items){
+			include 'invform.php';
+			exit;
 		}
 		else {
-			$error = 'could not find business';
+			$error = 'could not get items';
 		}
-		include 'invform.php';
+	}
+	else {
+		$error = 'could not find business';
+	}
+	include 'invform.php';
+	exit;
+}
+elseif (isset($_GET['busName']) && $_GET['busName'] != ''){
+	$busName = $_GET['busName'];
+	$_SESSION['busName'] = $busName;
+	$business=getBus($busName);
+
+	if ($business) {
+		$busName = $business['name'];
+		$busDesc = $business['description'];
+		$busId = $business['businessId'];
+		$invId = $business['inventoryId'];
+		$items = getItems($invId);
+		$_SESSION['items'] = $items;
+		$_SESSION['busId'] = $busId;
+		if ($_GET['action'] === 'edit'){
+			include 'editBus.php';
+			exit;
+		}
+		if($items){
+			include 'view.php';
+			exit;
+		}
+		else {
+			$error = 'No items found';
+			include 'view.php';
+			exit;
+		}
+	}
+	else {
+		echo 'No Businesses found!';
+	}
+
+}
+elseif ($_SESSION['loggedin'] == 'yes' || $_GET['action'] == 'select'){
+	$email = $_SESSION['user'];
+	$business = getBusNames($email);
+	$_SESSION['business'] = $business;
+	if ($business) {
+		$busName = $business['name'];
+		$busDesc = $business['description'];
+		$busId = $business['businessId'];
+		include 'selectbus.php';
 		exit;
 	}
-
-	if (isset($_GET['busName']) && $_GET['busName'] != ''){
-		$busName = $_GET['busName'];
-		$_SESSION['busName'] = $busName;
-		$business=getBus($busName);
-
-		if ($business) {
-			$busName = $business['name'];
-			$busDesc = $business['description'];
-			$busId = $business['businessId'];
-			$invId = $business['inventoryId'];
-			$items = getItems($invId);
-			$_SESSION['items'] = $items;
-			$_SESSION['busId'] = $busId;
-			if ($_GET['action'] === 'edit'){
-				include 'editBus.php';
-				exit;
-			}
-		if($items){
-				include 'view.php';
-				exit;
-			}
-			else {
-				$error = 'No items found';
-				include 'view.php';
-				exit;
-			}
-		}
-		else {
-			echo 'No Businesses found!';
-		}
-
+	else {
+		$error = 'No Businesses found!';
+		include 'selectbus.php';
+		exit;
 	}
-	elseif ($_SESSION['loggedin'] == 'yes' || $_GET['action'] == 'select'){
-		$email = $_SESSION['user'];
-		$business = getBusNames($email);
-		$_SESSION['business'] = $business;
-		if ($business) {
-			$busName = $business['name'];
-			$busDesc = $business['description'];
-			$busId = $business['businessId'];
-			include 'selectbus.php';
-			exit;
-		}
-		else {
-			$error = 'No Businesses found!';
-			include 'selectbus.php';
-			exit;
-		}
-	}
+}
 	// elseif ($_SESSION['loggedin'] == 'yes') {
 	// 	$email = $_SESSION['user'];
 	// 	$business = getBusNames($email);
@@ -212,39 +210,39 @@ else
 
 
 	// }
-	elseif ($_POST['submit'] == 'Login') {
-		$email = htmlspecialchars($_POST['email']);
-		$pass = hashPassword($_POST['password']);
+elseif ($_POST['submit'] == 'Login') {
+	$email = htmlspecialchars($_POST['email']);
+	$pass = hashPassword($_POST['password']);
 
-		$loginSuccess = login($email, $pass);
-		if($loginSuccess) {
-			$_SESSION['loggedin'] = 'yes';
-			$_SESSION['user'] = $email;
-			$_SESSION['accountId'] = $loginSuccess['accountId'];
-			$business = getBusNames($email);
-			$_SESSION['business'] = $business;
-			if ($business) {
-				$busName = $business['name'];
-				$busDesc = $business['description'];
-				$busId = $business['businessId'];
+	$loginSuccess = login($email, $pass);
+	if($loginSuccess) {
+		$_SESSION['loggedin'] = 'yes';
+		$_SESSION['user'] = $email;
+		$_SESSION['accountId'] = $loginSuccess['accountId'];
+		$business = getBusNames($email);
+		$_SESSION['business'] = $business;
+		if ($business) {
+			$busName = $business['name'];
+			$busDesc = $business['description'];
+			$busId = $business['businessId'];
 
-			}
-			else {
-				$error = "No Businesses found!";
-			}
-			include 'selectbus.php';
-			exit;
 		}
 		else {
-			$error = 'Sorry, the login failed.';
-			include 'login.php';
-			exit;
+			$error = "No Businesses found!";
 		}
+		include 'selectbus.php';
+		exit;
 	}
 	else {
-	//default view
-		var_dump($_POST);
-		echo 'default view';
+		$error = 'Sorry, the login failed.';
 		include 'login.php';
 		exit;
 	}
+}
+else {
+	//default view
+	var_dump($_POST);
+	echo 'default view';
+	include 'login.php';
+	exit;
+}
